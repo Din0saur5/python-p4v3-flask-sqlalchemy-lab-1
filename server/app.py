8cae4f64dcb1ab5b05e382c3387924e9b141f53f
Daniel Hackworth <dan.hackworth.515@gmail.com>
2023-12-05 15:03:57 -0500

    lab complete

--- server/app.py
+++ server/app.py
@@ -7,7 +7,7 @@ from flask_migrate import Migrate
 from models import db, Earthquake
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///earthquakes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
@@ -21,7 +21,26 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def res_by_id(id):
+    quake = Earthquake.query.filter(Earthquake.id == id).first()
+    
+    if quake != None:
+        response_body = quake.to_dict()
+        return make_response(response_body,200)
+    
+    return {"error": "Hotel Not Found!"}
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def by_mag(magnitude):
+    quakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    
+    if quakes != []:
+        response_body = [quake.to_dict() for quake in quakes]
+        return make_response(response_body,200)
+    
+    return {"error": "Hotel Not Found!"}
 
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5001, debug=True)
